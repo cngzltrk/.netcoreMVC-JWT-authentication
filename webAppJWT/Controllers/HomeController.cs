700682412164fa8919d5f9aa786d1700c3dc4eca
Cengiz Alturk <cngzltrk@gmail.com>
2019-09-18 12:30:26 +0300

    JWT authentication

---
+++ webAppJWT/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using webAppJWT.Models;
+
+namespace webAppJWT.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //[HttpGet("{pass},{username}")]
+        public IActionResult LoginUser(string username,string pass)
+        {
+            TokenProvider _tokenProvider = new TokenProvider();
+            //Authenticate user
+            var userToken = _tokenProvider.LoginUser(username.Trim(), pass.Trim());
+            if (userToken != null)
+            {
+                //Save token in session object
+                HttpContext.Session.SetString("JWToken", userToken);
+            }
+            return Redirect("~/Home/Index");
+        }
+        public IActionResult Logoff()
+        {
+            HttpContext.Session.Clear();
+            return Redirect("~/Home/Index");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ webAppJWT/Startup.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+
+namespace webAppJWT
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //session servisi ekleyip
+            services.AddSession();
+            var SecretKey = Encoding.ASCII.GetBytes
+                ("YourKey-2374-OFFKDI940NG7:56753253-tyuw-5769-0921-kfirox29zoxv");
+            services.AddAuthentication(auth =>
+            {
+                auth.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                auth.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+               .AddJwtBearer(token =>
+               {
+                   token.RequireHttpsMetadata = false;
+                   token.SaveToken = true;
+                   token.TokenValidationParameters = new TokenValidationParameters
+                   {
+                       ValidateIssuerSigningKey = true,
+                        //Same Secret key will be used while creating the token
+                        IssuerSigningKey = new SymmetricSecurityKey(SecretKey),
+                       ValidateIssuer = true,
+                        //Usually, this is your application base URL
+                        ValidIssuer = "http://localhost:54780",
+                       ValidateAudience = true,
+                        //Here, we are creating and using JWT within the same application.
+                        //In this case, base URL is fine.
+                        //If the JWT is created using a web service, then this would be the consumer URL.
+                        ValidAudience = "http://localhost:54780",
+                       RequireExpirationTime = true,
+                       ValidateLifetime = true,
+                       ClockSkew = TimeSpan.Zero
+                   };
+               });
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseSession();
+            
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+            //Add JWToken to all incoming HTTP Request Header
+            app.Use(async (context, next) =>
+            {
+                var JWToken = context.Session.GetString("JWToken");
+                if (!string.IsNullOrEmpty(JWToken))
+                {
+                    context.Request.Headers.Add("Authorization", "Bearer " + JWToken);
+                }
+                await next();
+            });
+            //Add JWToken Authentication service
+            app.UseAuthentication();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
